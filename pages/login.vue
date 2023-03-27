f4a126dc92c6ff276981862199cb42d74d7d6cce
Irving Aguilar <iaguilar@simetrical.com.mx>
2023-03-26 21:26:16 -0600

    first commit 🚀

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<template>
+  <div class="login-container">
+    <h1 class="title">Welcome 😀</h1>
+    <div class="inputs-container">
+      <label for="username">Username</label>
+      <input id="username" type="password" name="username" />
+    </div>
+    <div class="inputs-container">
+      <label for="password">Password</label>
+      <input id="password" type="password" name="password" />
+    </div>
+    <button class="btn btn-primary">Login now 🚀</button>
+  </div>
+</template>
